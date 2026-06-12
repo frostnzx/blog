762e8311f73364fc784d89b7f0bb09042bb4f71c
Siwakorn Sriveerakul <jeensiwakorn001@gmail.com>
2026-06-12 18:20:41 +0700

    add resume page

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const fileId = '1W0AL43_DmFl1Hn2NGWR3bcUJ0lhhaZua';
+
+	const previewUrl = `https://drive.google.com/file/d/${fileId}/preview`;
+	const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+</script>
+
+<div class="flex flex-row gap-5">
+	<iframe src={previewUrl} width="70%" height="850" title="PDF preview" />
+
+	<div class="flex flex-1 items-center justify-center">
+		<a href={downloadUrl} target="_blank" rel="noopener noreferrer" class="text-center underline">
+			Download PDF
+		</a>
+	</div>
+</div>
